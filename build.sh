5d2984224cfee7569d144dd5258119c7c3a52420
matt-grain <matt@grainecosystem.com>
2023-02-18 17:26:38 -0500

    testing...

---
+++ build.sh
@@ -0,0 +1,1 @@
+zig build-exe src/main.zig -I ./src ./src/libymfm/ymfm_misc.cpp ./src/lbymfm/src/ymfm_opl.cpp ./src/lbymfm/src/ymfm_opm.cpp ./src/lbymfm/src/ymfm_opn.cpp ./src/lbymfm/src/ymfm_opq.cpp ./src/lbymfm/src/ymfm_opz.cpp ./src/lbymfm/src/ymfm_adpcm.cpp ./src/lbymfm/src/ymfm_pcm.cpp ./src/lbymfm/src/ymfm_ssg.cpp
--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,13 @@ const ymfm = @cImport({
 });
 
 
-pub fn main() void {
+pub fn main() !void {
 
     const stdout = std.io.getStdOut().writer();
 
     const clock: u32 = 8000;
-    var res: u32 = ymfm.ymfm_add_chip(ymfm.chip_type.CHIP_YM2149, clock);
+    // var res: u32 = ymfm.ymfm_add_chip(ymfm.chip_type.CHIP_YM2149, clock);
+    var res: u32 = ymfm.ymfm_add_chip(1, clock);
 
     try stdout.print("Rssult, {}\n", .{res});
 }
